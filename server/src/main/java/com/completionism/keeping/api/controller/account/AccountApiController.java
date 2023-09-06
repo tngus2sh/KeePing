d9f15a9712c8fc4576ebf410b35078feb49ffffb
이진하 <ljh8190@naver.com>
2023-09-06 17:54:16 +0900

    feat: 계좌 생성 기능 skeleton controller, dto 파일 생성

--- server/src/main/java/com/completionism/keeping/api/controller/account/AccountApiController.java
+++ server/src/main/java/com/completionism/keeping/api/controller/account/AccountApiController.java
@@ -1,8 +1,12 @@
 package com.completionism.keeping.api.controller.account;
 
+import com.completionism.keeping.api.ApiResponse;
+import com.completionism.keeping.api.controller.account.request.AddAccountRequest;
 import com.completionism.keeping.api.service.account.AccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,8 @@ public class AccountApiController {
 
     private final AccountService accountService;
 
-
+    @PostMapping
+    public ApiResponse<Void> addAccount(@RequestBody AddAccountRequest request) {
+        return null;
+    }
 }
---
+++ server/src/main/java/com/completionism/keeping/api/controller/account/request/AddAccountRequest.java
@@ -0,0 +1,12 @@
+package com.completionism.keeping.api.controller.account.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class AddAccountRequest {
+}
