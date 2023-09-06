c18c5620bf83eee68c7db1032070fa9576cad290
오정원 <wjddnjs0802@ajou.ac.kr>
2023-09-06 20:35:51 +0900

    chore: client 디렉토리 구조 설정 (#S09P22C207-100)

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,62 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!!!'),
-        ),
-      ),
-    );
-  }
-}
-
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to the second screen when tapped.
-          },
-          child: const Text('Launch screen'),
-        ),
-      ),
-    );
-  }
-}
-
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first screen when tapped.
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(),
+      body: SizedBox(),
+      bottomNavigationBar: BottomAppBar(),
+    ));
   }
 }
