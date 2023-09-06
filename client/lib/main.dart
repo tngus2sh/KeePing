b4497a7492b977262f5563da95174438a7e9ce60
오정원 <wjddnjs0802@ajou.ac.kr>
2023-09-06 21:40:58 +0900

    feat: 하단네비게이션바 기본기능 구현 (#S09P22C207-100
    )

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
+import './screens/page1/page1.dart';
+import './screens/page2/page2.dart';
+import './screens/page3/page3.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return Scaffold(
       appBar: AppBar(),
-      body: SizedBox(child: Text('hello world!')),
-      bottomNavigationBar: BottomAppBar(),
-    ));
+      body: SizedBox(child: Text('hello world!?')),
+      bottomNavigationBar: BottomAppBar(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Page1()));
+            },
+            child: const Text('Page1!'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Page2()));
+            },
+            child: const Text('Page2!'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Page3()));
+            },
+            child: const Text('Page3!'),
+          )
+        ],
+      )),
+    );
   }
 }
