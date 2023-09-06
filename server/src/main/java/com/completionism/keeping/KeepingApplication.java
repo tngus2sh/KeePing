1f314e8d275ca8315ef818bc0ec7504d0be6d559
이진하 <ljh8190@naver.com>
2023-09-06 09:17:26 +0900

    chore: 백엔드 프로젝트 생성, 기본 의존성 추가

---
+++ server/src/main/java/com/completionism/keeping/KeepingApplication.java
@@ -0,0 +1,13 @@
+package com.completionism.keeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepingApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/completionism/keeping/KeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.completionism.keeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
