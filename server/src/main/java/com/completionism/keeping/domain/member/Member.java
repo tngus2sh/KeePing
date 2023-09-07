b142fdc38aefb6e98d79dd9c0dac3f8da1fda349
이예리 <leeyr0412@naver.com>
2023-09-07 14:19:46 +0900

    feat: Member Entity 생성 (#S09P22C207-16)

---
+++ server/src/main/java/com/completionism/keeping/domain/member/Member.java
@@ -0,0 +1,52 @@
+package com.completionism.keeping.domain.member;
+
+import com.completionism.keeping.global.common.TimeBaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Member extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    @Size(max = 20, min = 5)
+    private String loginId;
+
+    @Column(nullable = false, length = 25)
+    @Size(max = 25, min = 5)
+    private String encryptionPw;
+
+    private String name;
+    private String phone;
+    private LocalDateTime birth;
+    private String profileImage;
+    private String fcmToken;
+    private boolean active;
+
+    @Builder
+    private Member(Long id, String loginId, String encryptionPw, String name, String phone, LocalDateTime birth, String profileImage, String fcmToken, boolean active) {
+        this.id = id;
+        this.loginId = loginId;
+        this.encryptionPw = encryptionPw;
+        this.name = name;
+        this.phone = phone;
+        this.birth = birth;
+        this.profileImage = profileImage;
+        this.fcmToken = fcmToken;
+        this.active = active;
+    }
+}
