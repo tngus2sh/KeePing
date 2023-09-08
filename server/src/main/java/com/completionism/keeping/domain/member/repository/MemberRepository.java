55e6182e5a83703cec9cd6677d49d5a6efdc197a
이예리 <leeyr0412@naver.com>
2023-09-08 17:10:56 +0900

    feat: MemberRepository 멤버찾기 구현 (#S09P22C207-16)

--- server/src/main/java/com/completionism/keeping/domain/member/repository/MemberRepository.java
+++ server/src/main/java/com/completionism/keeping/domain/member/repository/MemberRepository.java
@@ -4,6 +4,10 @@ import com.completionism.keeping.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId();
 }
