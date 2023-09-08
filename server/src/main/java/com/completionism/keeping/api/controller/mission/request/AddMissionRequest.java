b093c76988264ef1f4562db8457274cdf169aa85
소수현 <ssuhyeon98@gmail.com>
2023-09-08 15:41:25 +0900

    refactor: AddMissionRequest 미션 등록 객체 유효성 검사 추가 (#S09P22C207-124)

--- server/src/main/java/com/completionism/keeping/api/controller/mission/request/AddMissionRequest.java
+++ server/src/main/java/com/completionism/keeping/api/controller/mission/request/AddMissionRequest.java
@@ -7,24 +7,40 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 public class AddMissionRequest {
 
+    @NotBlank
+    @Size(min = 5, max = 6)
+    @Pattern(regexp = "^(PARENT|CHILD)$")
     private MissionType type; // 부모가 아이에게, 아이가 부모에게
     
+    @NotNull
+    @Size(max = 10)
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(\\d*)[a-zA-Z\\d]{5,10}$")
     private String to; // 어떤 아이한테 보내야하는지
 
+    @NotBlank
+    @Size(min = 0)
     private String todo; // 미션 내용
 
+    @NotNull
+    @Positive
+    @Size(min = 10, max = 10000)
     private int money; // 미션 보상금
     
     private String cheeringMessage; // 부모 응원 메시지
 
+    @NotBlank
+    @FutureOrPresent
     private LocalDate startDate; // 미션 시작일
 
+    @NotBlank
+    @FutureOrPresent
     private LocalDate endDate; // 미션 마감일
 
     @Builder
