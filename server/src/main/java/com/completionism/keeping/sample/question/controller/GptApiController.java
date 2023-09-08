a52627acbc3f04b26bbe6a41fe50184f047781e7
소수현 <ssuhyeon98@gmail.com>
2023-09-08 11:45:12 +0900

    feat: gpt-3.5 연결 API 작성 (#S09P22C207-136)

--- server/src/main/java/com/completionism/keeping/sample/question/controller/GptApiController.java
+++ server/src/main/java/com/completionism/keeping/sample/question/controller/GptApiController.java
@@ -1,31 +1,23 @@
 package com.completionism.keeping.sample.question.controller;
 
-import com.completionism.keeping.api.ApiResponse;
+import com.completionism.keeping.sample.question.controller.request.GPTCompletionChatRequest;
+import com.completionism.keeping.sample.question.controller.response.CompletionChatResponse;
 import com.completionism.keeping.sample.question.service.GptService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api")
 public class GptApiController {
     
-    private GptService gptService;
-    
-    @PostMapping("/api/gpt")
-    public ApiResponse<String> chat(
-            @RequestBody String content
-    ) {
-        try {
-            String chat = gptService.chat(content);
-            log.info("chat: " + chat);
-            return ApiResponse.ok(chat);
-        } catch (Exception e) {
-            return ApiResponse.of(404, HttpStatus.NOT_FOUND, "오류 발생", null);
-        }
+    private final GptService gptService;
+
+    @PostMapping("/gpt")
+    public CompletionChatResponse completionChat(final @RequestBody GPTCompletionChatRequest request) {
+        log.info("request={}", request);
+        return gptService.completionChat(request);
     }
 }
