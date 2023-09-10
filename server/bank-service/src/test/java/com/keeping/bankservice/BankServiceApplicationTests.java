c33a4bebf6df653a1240d24992dc3c3604755211
이예리 <leeyr0412@naver.com>
2023-09-11 01:05:55 +0900

    backup: MSA setting backup (#S09P22C207-182)

---
+++ server/bank-service/src/test/java/com/keeping/bankservice/BankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keeping.bankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ server/mission-service/src/main/java/com/keeping/missionservice/MissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.keeping.missionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionServiceApplication.class, args);
+	}
+
+}
