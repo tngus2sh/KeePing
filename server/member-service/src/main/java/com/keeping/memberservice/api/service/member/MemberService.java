7fa54a094723ace7907bc595baf9bdb70c02f875
이예리 <leeyr0412@naver.com>
2023-09-11 14:39:40 +0900

    feat: 부모 회원가입 Service 골격 생성 (#S09P22C207-26)

--- server/member-service/src/main/java/com/keeping/memberservice/api/service/member/MemberService.java
+++ server/member-service/src/main/java/com/keeping/memberservice/api/service/member/MemberService.java
@@ -1,23 +1,22 @@
-//package com.keeping.memberservice.api.service.member;
-//
-//import com.completionism.keeping.api.service.member.dto.AddParentDto;
-//import com.completionism.keeping.domain.member.repository.MemberRepository;
-//import com.completionism.keeping.domain.member.repository.ParentRepository;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@RequiredArgsConstructor
-//@Service
-//@Transactional
-//@Slf4j
-//public class MemberService {
-//
-//    private final ParentRepository parentRepository;
-//    private final MemberRepository memberRepository;
-//
-//    public String addParent(AddParentDto dto){
-//        return "";
-//    }
-//}
+package com.keeping.memberservice.api.service.member;
+
+import com.keeping.memberservice.api.service.member.dto.AddParentDto;
+import com.keeping.memberservice.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class MemberService {
+
+    //    private final ParentRepository parentRepository;
+    private final MemberRepository memberRepository;
+
+    public String addParent(AddParentDto dto) {
+        return "";
+    }
+}
