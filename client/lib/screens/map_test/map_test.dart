2fb402127c9d7023319e5f9d8758d9cebd13d53e
윤예지 <hallo27@naver.com>
2023-09-11 16:51:05 +0900

    feat: Google Map 마커 및 다중선 추가 (#S09P22C207-165)

--- client/lib/screens/map_test/map_test.dart
+++ client/lib/screens/map_test/map_test.dart
@@ -12,10 +12,49 @@ class MapTest extends StatefulWidget {
 class _MapTestState extends State<MapTest> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  // 임시 더미데이터 
+  final _places = [
+    {
+      "date": "2023-09-07",
+      'store_name': '싸피',
+      'latitude': 35.205472,
+      'longitude': 126.811582,
+    },
+    {
+      "date": "2023-09-07",
+      'store_name': '컴포즈 커피',
+      'latitude': 35.200478,
+      'longitude': 126.814354,
+    },
+    {
+      "date": "2023-09-07",
+      'store_name': '스타벅스',
+      'latitude': 35.203040,
+      'longitude': 126.818698,
+    },
+  ];
+
+  final _markers = <Marker>{};
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  @override
+  void initState() {
+    _markers.addAll(
+      _places.map(
+        (e) => Marker(
+          markerId: MarkerId(e['name'] as String),
+          infoWindow: InfoWindow(title: e['store_name'] as String),
+          position: LatLng(
+            e['latitude'] as double,
+            e['longitude'] as double,
+          ),
+        ),
+      ),
+    );
+    super.initState();
   }
 
   @override
@@ -30,10 +69,20 @@ class _MapTestState extends State<MapTest> {
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+            target: LatLng(_places.first['latitude'] as double, _places.first['longitude'] as double),
+            zoom: 17.0,
           ),
-        ),
+          markers: _markers,
+          polylines: {Polyline(
+            polylineId: PolylineId('9월 1일'),
+            points: _places.map((e) => LatLng(e['latitude'] as double, e['longitude'] as double)).toList(),
+            color: Colors.blue,
+            startCap: Cap.roundCap,
+            endCap: Cap.roundCap,
+            width: 5,
+            jointType: JointType.round
+          )}
+        )
       ),
     );
   }
