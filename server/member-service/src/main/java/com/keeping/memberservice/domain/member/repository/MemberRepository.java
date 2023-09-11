1528d2d501951979c9c416f6327d7b7195123b3f
이예리 <leeyr0412@naver.com>
2023-09-11 10:16:25 +0900

    feat: MSA MemberService Project setting (#S09P22C207-182)

---
+++ server/member-service/src/main/java/com/keeping/memberservice/domain/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+//package com.keeping.memberservice.domain.member.repository;
+//
+//import com.completionism.keeping.domain.member.Member;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface MemberRepository extends JpaRepository<Member, Long> {
+//
+//    Optional<Member> findByLoginId();
+//}
