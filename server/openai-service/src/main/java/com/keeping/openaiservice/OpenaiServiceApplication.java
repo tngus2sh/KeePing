ff3d565435e8b9c6bb5acdbc93eefeefb713da28
이예리 <leeyr0412@naver.com>
2023-09-11 10:24:20 +0900

    feat: MSA OpenaiService Project setting (#S09P22C207-182)

--- server/openai-service/src/main/java/com/keeping/openaiservice/OpenaiServiceApplication.java
+++ server/openai-service/src/main/java/com/keeping/openaiservice/OpenaiServiceApplication.java
@@ -2,8 +2,10 @@ package com.keeping.openaiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OpenaiServiceApplication {
 
 	public static void main(String[] args) {
