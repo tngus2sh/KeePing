3a1e5006a7e7fb1ee724f591e6dc8ab75991c730
이예리 <leeyr0412@naver.com>
2023-09-12 11:53:04 +0900

    feat: 문자인증 확인 Controller 작성 (#S09P22C207-26)

--- server/member-service/src/main/java/com/keeping/memberservice/api/ApiResponse.java
+++ server/member-service/src/main/java/com/keeping/memberservice/api/ApiResponse.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public class ApiResponse<T> {
-    
+
     private DataHeader resultStatus;
     private T resultBody;
 
@@ -14,8 +14,8 @@ public class ApiResponse<T> {
         this.resultBody = dataBody;
     }
 
-    public static <T> ApiResponse<T> of(int successCode, HttpStatus status, String resultMessage, T data) {
-        return new ApiResponse<>(new DataHeader(successCode, String.valueOf(status.value()), resultMessage), data);
+    public static <T> ApiResponse<T> of(int successCode, HttpStatus status, String resultMessage) {
+        return new ApiResponse<>(new DataHeader(successCode, String.valueOf(status.value()), resultMessage), null);
     }
 
     public static <T> ApiResponse<T> ok(T data) {
