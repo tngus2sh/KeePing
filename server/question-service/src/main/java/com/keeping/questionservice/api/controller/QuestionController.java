3fbde00160f9ce27f03f026a499ce2f6be60f351
이예리 <leeyr0412@naver.com>
2023-09-14 21:55:32 +0900

    feat: 질문 조회 (#S09P22C207-181)

--- server/question-service/src/main/java/com/keeping/questionservice/api/controller/QuestionController.java
+++ server/question-service/src/main/java/com/keeping/questionservice/api/controller/QuestionController.java
@@ -2,6 +2,7 @@ package com.keeping.questionservice.api.controller;
 
 import com.keeping.questionservice.api.ApiResponse;
 import com.keeping.questionservice.api.controller.request.AddQuestionRequest;
+import com.keeping.questionservice.api.controller.response.QuestionResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,13 @@ import javax.validation.Valid;
 @RequestMapping("/question-service/{memberKey}")
 @Slf4j
 public class QuestionController {
+
+
+    @GetMapping("/questions/{date}")
+    private ApiResponse<QuestionResponse> getQuestion(@PathVariable String memberKey,
+                                                      @PathVariable String date) {
+        return ApiResponse.ok(QuestionResponse.builder().build());
+    }
 
     @PostMapping
     public ApiResponse<String> addQuestion(@RequestBody @Valid AddQuestionRequest request,
