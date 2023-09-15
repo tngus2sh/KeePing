c387641bb9296e8e3957bfe7c19f2458092af1ab
윤예지 <hallo27@naver.com>
2023-09-15 09:16:42 +0900

    feat: 계좌 개설, 저금통 개설 샘플 코드 일부 함수 및 스타일 코드 분리 (#S09P22C207-169)

---
+++ client/lib/screens/sample_code_page/utils/styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+ButtonStyle authenticationBtnStyle() {
+  return ButtonStyle(
+    backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+    foregroundColor: MaterialStateProperty.all<Color>(const Color(0xFF8320E7)),
+    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+        side: BorderSide(
+          color: const Color(0xFF8320E7), // 테두리 색상 설정
+          width: 2.0, // 테두리 두께 설정
+        ),
+      )
+    ),
+    fixedSize: MaterialStateProperty.all<Size>(
+      Size(180, 40)
+    )
+  );
+}
