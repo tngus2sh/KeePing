a81a66a067ff6764ddd381a42db3b928c933f3f8
hallo27 <hallo27@naver.com>
2023-09-19 00:19:54 +0900

    feat: 자녀 저금통 메인 화면 필터 버튼 및 플로팅 버튼 추가 (#S09P22C207-215)

--- client/lib/screens/piggy_page/piggy_page.dart
+++ client/lib/screens/piggy_page/piggy_page.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:keeping/screens/piggy_page/make_piggy_test.dart';
+import 'package:keeping/screens/piggy_page/widgets/piggy_filters.dart';
+import 'package:keeping/screens/piggy_page/widgets/piggy_info.dart';
+import 'package:keeping/widgets/bottom_nav.dart';
+import 'package:keeping/widgets/floating_btn.dart';
+import 'package:keeping/widgets/header.dart';
 
 class PiggyPage extends StatelessWidget {
   PiggyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: MyHeader(
+        text: '\u{1F437} 저금통',
+        bgColor: const Color(0xFF8320E7),
+        elementColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          PiggyInfo(),
+          PiggyFilters(),
+        ],
+      ),
+      floatingActionButton: FloatingBtn(
+        text: '만들기',
+        icon: Icon(Icons.savings_rounded),
+        path: MakePiggyTest(),
+      ),
+      bottomNavigationBar: BottomNav(),
+    );
   }
 }
