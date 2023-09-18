b176542b54adf3138bddf4d0bc98594f83a5aeb0
hallo27 <hallo27@naver.com>
2023-09-19 01:06:27 +0900

    feat: 자녀 저금통 메인 페이지 저금통 목록 추가 (#S09P22C207-215)

--- client/lib/screens/piggy_page/piggy_page.dart
+++ client/lib/screens/piggy_page/piggy_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:keeping/screens/piggy_page/make_piggy_test.dart';
 import 'package:keeping/screens/piggy_page/widgets/piggy_filters.dart';
 import 'package:keeping/screens/piggy_page/widgets/piggy_info.dart';
+import 'package:keeping/screens/piggy_page/widgets/piggy_info_card.dart';
 import 'package:keeping/widgets/bottom_nav.dart';
 import 'package:keeping/widgets/floating_btn.dart';
 import 'package:keeping/widgets/header.dart';
@@ -21,6 +22,33 @@ class PiggyPage extends StatelessWidget {
         children: [
           PiggyInfo(),
           PiggyFilters(),
+          Expanded(
+            child: Container(
+              decoration: piggyPageBgStyle(),
+              width: double.infinity,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    PiggyInfoCard(
+                      balance: 74000,
+                      content: '아디다스 삼바 내꺼야',
+                      goalMoney: 140000,
+                    ),
+                    PiggyInfoCard(
+                      balance: 74000,
+                      content: '아디다스 삼바 내꺼야',
+                      goalMoney: 140000,
+                    ),
+                    PiggyInfoCard(
+                      balance: 74000,
+                      content: '아디다스 삼바 내꺼야',
+                      goalMoney: 140000,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
         ],
       ),
       floatingActionButton: FloatingBtn(
@@ -31,4 +59,10 @@ class PiggyPage extends StatelessWidget {
       bottomNavigationBar: BottomNav(),
     );
   }
+}
+
+BoxDecoration piggyPageBgStyle() {
+  return BoxDecoration(
+    color: const Color(0xFFFAFAFA),
+  );
 }
