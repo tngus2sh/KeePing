2dbf18fcb8005561c3223e33ffec56de24086c37
이진하 <ljh8190@naver.com>
2023-09-22 00:57:08 +0900

    feat: 상세 거래 내역 등록 controller 작성 (#S09P22C207-270)

--- server/bank-service/src/main/java/com/keeping/bankservice/api/controller/account_detail/AccountDetailApiController.java
+++ server/bank-service/src/main/java/com/keeping/bankservice/api/controller/account_detail/AccountDetailApiController.java
@@ -4,6 +4,8 @@ import com.keeping.bankservice.api.ApiResponse;
 import com.keeping.bankservice.api.controller.account_detail.request.AddAccountDetailRequest;
 import com.keeping.bankservice.api.service.account_detail.AccountDetailService;
 import com.keeping.bankservice.api.service.account_detail.dto.AddAccountDetailDto;
+import com.keeping.bankservice.global.exception.InvalidRequestException;
+import com.keeping.bankservice.global.exception.NoAuthorizationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -22,11 +24,15 @@ public class AccountDetailApiController {
 
         AddAccountDetailDto dto = AddAccountDetailDto.toDto(request);
 
-//        try {
-//            accountDetailService.addAccountDetail(memberKey, dto);
-//        }
+        try {
+            accountDetailService.addAccountDetail(memberKey, dto);
+        } catch (NoAuthorizationException e) {
+            return ApiResponse.of(1, e.getHttpStatus(), e.getResultMessage(), null);
+        } catch (InvalidRequestException e) {
+            return ApiResponse.of(1, e.getHttpStatus(), e.getResultMessage(), null);
+        }
 
-        return null;
+        return ApiResponse.ok(null);
     }
 
 }
