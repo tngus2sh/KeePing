a3a439f56022b438d94446b2b14c65b241575572
tngus2sh <ssuhyeon98@gmail.com>
2023-09-24 14:34:42 +0900

    refactor: 미션 성공시 출금 처리 #S09P22C207-261

--- server/mission-service/src/main/java/com/keeping/missionservice/api/service/mission/impl/MissionServiceImpl.java
+++ server/mission-service/src/main/java/com/keeping/missionservice/api/service/mission/impl/MissionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.keeping.missionservice.api.service.mission.impl;
 
+import com.keeping.missionservice.api.ApiResponse;
 import com.keeping.missionservice.api.controller.mission.BankFeignClient;
 import com.keeping.missionservice.api.controller.mission.MemberFeignClient;
 import com.keeping.missionservice.api.controller.mission.NotiFeignClient;
@@ -60,8 +61,8 @@ public class MissionServiceImpl implements MissionService {
             }
 
             // 부모의 계좌에 들어있는 금액 한도 내에서 가능
-            AccountResponse parentBalance = bankFeignClient.getAccountBalanceFromParent(memberKey);
-            int limitAmount = parentBalance.getBalance();
+            ApiResponse<AccountResponse> accountBalanceFromParent = bankFeignClient.getAccountBalanceFromParent(memberKey);
+            int limitAmount = accountBalanceFromParent.getDataBody().getBalance();
 
             // 현재 완료하지 않은 미션 총액
             Optional<Integer> missionTotalMoney = missionQueryRepository.countMoney(dto.getTo());
@@ -165,8 +166,8 @@ public class MissionServiceImpl implements MissionService {
                     && dto.getCompleted().equals(Completed.YET)) {
 
                 // 부모 통장의 잔액과 미션 총액을 비교
-                AccountResponse parentBalance = bankFeignClient.getAccountBalanceFromParent(memberKey);
-                int limitAmount = parentBalance.getBalance();
+                ApiResponse<AccountResponse> accountBalanceFromParent = bankFeignClient.getAccountBalanceFromParent(memberKey);
+                int limitAmount = accountBalanceFromParent.getDataBody().getBalance();
                 int totalMissionMoney = 0;
 
                 // 아이들 목록 불러오기
@@ -182,7 +183,7 @@ public class MissionServiceImpl implements MissionService {
 
                 if (totalMissionMoney < limitAmount) {
 
-                    throw new AlreadyExistException("409", HttpStatus.CONFLICT, "잔액보다 미션 총액이 많습니다.");
+                    throw new AlreadyExistException("400", HttpStatus.BAD_REQUEST, "잔액보다 미션 총액이 많습니다.");
                 }
 
                 // cheeringMessage 추가
@@ -193,10 +194,13 @@ public class MissionServiceImpl implements MissionService {
             else if (mission.getCompleted().equals(Completed.FINISH_WAIT)
                     && dto.getCompleted().equals(Completed.FINISH)) {
 
+                // 부모 계좌에서 아이 계좌로 돈 이동
+                bankFeignClient.transferMoneyForMission(memberKey, mission.getMoney());
+
                 mission.updateCompleted(dto.getCompleted());
             }
             else {
-                throw new AlreadyExistException("409", HttpStatus.CONFLICT, "완성 상태를 바꿀 수 없습니다.");
+                throw new AlreadyExistException("400", HttpStatus.BAD_REQUEST, "완성 상태를 바꿀 수 없습니다.");
             }
         }
         // 자녀라면 YET -> FINISH_WAIT
@@ -224,8 +228,8 @@ public class MissionServiceImpl implements MissionService {
                 .orElseThrow(() -> new NotFoundException("404", HttpStatus.NOT_FOUND, "해당하는 미션을 찾을 수 없습니다."));
 
         // 부모 통장의 잔액과 미션 총액을 비교
-        AccountResponse parentBalance = bankFeignClient.getAccountBalanceFromParent(memberKey);
-        int limitAmount = parentBalance.getBalance();
+        ApiResponse<AccountResponse> accountBalanceFromParent = bankFeignClient.getAccountBalanceFromParent(memberKey);
+        int limitAmount = accountBalanceFromParent.getDataBody().getBalance();
         int totalMissionMoney = 0;
 
         // 아이들 목록 불러오기
