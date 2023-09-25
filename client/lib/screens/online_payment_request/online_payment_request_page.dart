6eafcc065a074d73ac74cb2c6885ae72d36a7420
hallo27 <hallo27@naver.com>
2023-09-25 23:59:39 +0900

    feat: 자녀 온라인 결제 요청 관련 dio 메서드 수정 (#S09P22C207-285)

--- client/lib/screens/online_payment_request/online_payment_request_page.dart
+++ client/lib/screens/online_payment_request/online_payment_request_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:keeping/provider/user_info.dart';
 import 'package:keeping/screens/online_payment_request/make_online_payment_request_page.dart';
 import 'package:keeping/screens/online_payment_request/online_payment_request_detail_page.dart';
+import 'package:keeping/screens/online_payment_request/utils/online_payment_request_future_methods.dart';
 import 'package:keeping/screens/online_payment_request/widgets/online_payment_request_filters.dart';
 import 'package:keeping/screens/online_payment_request/widgets/online_payment_request_info.dart';
 import 'package:keeping/styles.dart';
@@ -8,6 +10,7 @@ import 'package:keeping/widgets/bottom_nav.dart';
 import 'package:keeping/widgets/color_info_card.dart';
 import 'package:keeping/widgets/floating_btn.dart';
 import 'package:keeping/widgets/header.dart';
+import 'package:provider/provider.dart';
 
 final _tempData = {
   "online": [
@@ -84,12 +87,20 @@ class OnlinePaymentRequestPage extends StatefulWidget {
 }
 
 class _OnlinePaymentRequestPageState extends State<OnlinePaymentRequestPage> {
+  String? type;
+  String? accessToken;
+  String? memberKey;
+  String? childKey;
+
   List<Map<String, dynamic>> _response = [];
 
   @override
   void initState() {
     super.initState();
     _response = _tempData['online'] ?? [];
+    type = context.read<UserInfoProvider>().type;
+    // accessToken = context.read<UserInfoProvider>().accessToken;
+    // memberKey = context.read<UserInfoProvider>().memberKey;
   }
 
   @override
@@ -104,35 +115,55 @@ class _OnlinePaymentRequestPageState extends State<OnlinePaymentRequestPage> {
         children: [
           OnlinePaymentRequestInfo(),
           OnlinePaymentRequestFilters(),
-          Expanded(
-            child: Container(
-              decoration: lightGreyBgStyle(),
-              width: double.infinity,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    ..._response.map((e) => 
-                      ColorInfoCard(
-                        name: e['name'],
-                        url: e['url'],
-                        reason: e['reason'],
-                        cost: e['cost'],
-                        paidMoney: e['paidMoney'],
-                        status: e['status'],
-                        createdDate: DateTime.parse(e['createdDate']),
-                        path: OnlinePaymentRequestDetailPage(onlineId: e['id']),
-                      )                    
-                    ),
-                    // ColorInfoCard(path: OnlinePaymentRequestDetailPage()),
-                  ],
-                )
-              )
-            )
-
-          )
+          FutureBuilder(
+            future: accessToken == null && memberKey == null ? null :
+              type == 'PARENT' ? 
+              getOnlinePaymentRequestListForParent(accessToken: accessToken!, memberKey: memberKey!, childKey: childKey!)
+              : getOnlinePaymentRequestListForChild(accessToken: accessToken!, memberKey: memberKey!),
+            builder: (context, snapshot) {
+              print('스냅샷스냅샷스냅샷 ${snapshot.toString()}');
+              // if (snapshot.connectionState == ConnectionState.waiting) {
+              //   return const Text('로딩중');
+              // } else if (snapshot.connectionState == ConnectionState.done) {
+              //   if (snapshot.hasError) {
+              //     return const Text('스냅샷에 에러 발생');
+              //   } else if (snapshot.hasData) {
+                  return Expanded(
+                    child: Container(
+                      decoration: lightGreyBgStyle(),
+                      width: double.infinity,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            ..._response.map((e) => 
+                              ColorInfoCard(
+                                name: e['name'],
+                                url: e['url'],
+                                reason: e['reason'],
+                                cost: e['cost'],
+                                paidMoney: e['paidMoney'],
+                                status: e['status'],
+                                createdDate: DateTime.parse(e['createdDate']),
+                                path: OnlinePaymentRequestDetailPage(onlineId: e['id']),
+                              )                    
+                            ),
+                            // ColorInfoCard(path: OnlinePaymentRequestDetailPage()),
+                          ],
+                        )
+                      )
+                    )
+                  );
+              //   } else {
+              //     return const Text('스냅샷 데이터 없음');
+              //   }
+              // } else {
+              //   return Text('퓨처 객체 null');
+              // }
+            },
+          ),
         ],
       ),
-      floatingActionButton: FloatingBtn(
+      floatingActionButton: type == 'PARENT' ? null : FloatingBtn(
         text: '부탁하기',
         icon: Icon(Icons.face),
         path: MakeOnlinePaymentRequestFirstPage(),
