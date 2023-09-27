23c414073a63eaedd05aa127ce1bcb1941a3e6a4
윤예지 <hallo27@naver.com>
2023-09-28 00:44:04 +0900

    feat: 온라인 결제 요구 관련 dio 메서드 수정 (#S09P22C207-284)

--- client/lib/screens/online_payment_request/utils/online_payment_request_future_methods.dart
+++ client/lib/screens/online_payment_request/utils/online_payment_request_future_methods.dart
@@ -1,66 +1,42 @@
 import 'package:keeping/util/dio_method.dart';
 
-// 자녀 - 온라인 결제 전체조회
-Future<dynamic> getOnlinePaymentRequestListForChild({
-  required String accessToken, required String memberKey
+// 온라인 결제 전체조회
+Future<dynamic> getOnlinePaymentRequestList({
+  required String? accessToken, required String? memberKey, required String? targetKey,
 }) async {
-  try {
-    final response = await dioGet(
-      accessToken: accessToken,
-      url: '/bank-service/online/$memberKey',
-    );
-    print('자녀 온라인 결제 전체조회 응답 $response');
-    return response;
-  } catch (e) {
-    print('자녀 온라인 결제 전체조회 에러 $e');
+  if (accessToken != null && memberKey != null && targetKey != null) {
+    try {
+      final response = await dioGet(
+        accessToken: accessToken,
+        url: '/bank-service/api/$memberKey/online/$targetKey',
+      );
+      print('온라인 결제 전체조회 응답 $response');
+      return response;
+    } catch (e) {
+      print('온라인 결제 전체조회 에러 $e');
+    }
+  } else {
+    return;
   }
 }
 
-// 자녀 - 필터링된 온라인 결제 전체조회
+// 필터링된 온라인 결제 전체조회
 Future<dynamic> getFilteredOnlinePaymentRequestListForChild({
-  required String accessToken, required String memberKey, required String childKey, required String status
+  required String? accessToken, required String? memberKey, required String? targetKey, required String? status
 }) async {
-  try {
-    final response = await dioGet(
-      accessToken: accessToken,
-      url: '/bank-service/online/$memberKey/$childKey/$status',
-    );
-    print('자녀 필터링된 온라인 결제 전체조회 응답 $response');
-    return response;
-  } catch (e) {
-    print('자녀 필터링된 온라인 결제 전체조회 에러 $e');
-  }
-}
-
-// 부모 - 온라인 결제 전체조회
-Future<dynamic> getOnlinePaymentRequestListForParent({
-  required String accessToken, required String memberKey, required String childKey
-}) async {
-  try {
-    final response = await dioGet(
-      accessToken: accessToken,
-      url: '/bank-service/online/parent/$memberKey/$childKey',
-    );
-    print('부모 온라인 결제 전체조회 응답 $response');
-    return response;
-  } catch (e) {
-    print('부모 온라인 결제 전체조회 에러 $e');
-  }
-}
-
-// 부모 - 필터링된 온라인 결제 전체조회
-Future<dynamic> getFilteredOnlinePaymentRequestListForParent({
-  required String accessToken, required String memberKey, required String childKey, required String status
-}) async {
-  try {
-    final response = await dioGet(
-      accessToken: accessToken,
-      url: '/bank-service/online/parent/$memberKey/$childKey/$status',
-    );
-    print('부모 필터링된 온라인 결제 전체조회 응답 $response');
-    return response;
-  } catch (e) {
-    print('부모 필터링된 온라인 결제 전체조회 에러 $e');
+  if (accessToken != null && memberKey != null && targetKey != null && status != null) {
+    try {
+      final response = await dioGet(
+        accessToken: accessToken,
+        url: '/bank-service/api/$memberKey/online/$targetKey/$status',
+      );
+      print('필터링된 온라인 결제 전체조회 응답 $response');
+      return response;
+    } catch (e) {
+      print('필터링된 온라인 결제 전체조회 에러 $e');
+    }
+  } else {
+    return;
   }
 }
 
@@ -82,49 +58,55 @@ Future<dynamic> getOnlinePaymentRequestDetail({
 
 // 자녀 - 온라인 결제 요구
 Future<dynamic> createOnlinePaymentRequest({
-  required String accessToken,
-  required String memberKey, required String name, required String url, required String reason, required int cost, required int paidMoney
+  required String? accessToken, required String? memberKey,
+  required String? productName, required String? url, required String? content, required int? totalMoney, required int? childMoney
 }) async {
-  try {
-    final response = await dioPost(
-      accessToken: accessToken,
-      url: '/bank-service/online',
-      data: {
-        "memberKey": memberKey,
-        "name": name,
-        "url": url,
-        "reason": reason,
-        "cost": cost,
-        "paidMoney": paidMoney,
-        "status": "YET"
-      }
-    );
-    print('자녀 온라인 결제 요구 응답 $response');
-    return response;
-  } catch (e) {
-    print('자녀 온라인 결제 요구 에러 $e');
+  if (accessToken != null && memberKey != null && productName != null && url != null && content != null && totalMoney != null && childMoney != null) {
+    try {
+      final response = await dioPost(
+        accessToken: accessToken,
+        url: '/bank-service/api/$memberKey/online',
+        data: {
+          "productName": productName,
+          "url": url,
+          "content": content,
+          "totalMoney": totalMoney,
+          "childMoney": childMoney,
+        }
+      );
+      print('자녀 온라인 결제 요구 응답 $response');
+      return response;
+    } catch (e) {
+      print('자녀 온라인 결제 요구 에러 $e');
+    }
+  } else {
+    return;
   }
 }
 
 // 부모 = 온라인 결제 승인
 Future<dynamic> approveOnlinePaymentRequest({
-  required String accessToken,
-  required String memberKey, required String childKey, String? message,
+  required String? accessToken, required String? memberKey, 
+  required int? onlineId, required String? childKey, String? comment,
 }) async {
-  try {
-    final response = await dioPost(
-      accessToken: accessToken,
-      url: '/bank-service/online/approve',
-      data: {
-        "memberKey": memberKey,
-        "childKey": childKey,
-        "status": message != null ? 'REJECT' : 'ACCEPT',
-        "message": message
-      }
-    );
-    print('부모 온라인 결제 승인 응답 $response');
-    return response;
-  } catch (e) {
-    print('부모 온라인 결제 승인 에러 $e');
+  if (accessToken != null && memberKey != null && onlineId != null && childKey != null) {
+    try {
+      final response = await dioPost(
+        accessToken: accessToken,
+        url: '/bank-service/api/$memberKey/online/approve',
+        data: {
+          "onlineId": memberKey,
+          "childKey": childKey,
+          "approve": comment != null ? 'REJECT' : 'APPROVE',
+          "comment": comment
+        }
+      );
+      print('부모 온라인 결제 승인 응답 $response');
+      return response;
+    } catch (e) {
+      print('부모 온라인 결제 승인 에러 $e');
+    }
+  } else {
+    return;
   }
 }
