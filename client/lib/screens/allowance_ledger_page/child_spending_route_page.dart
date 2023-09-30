53f45e9ab41c832e1dc36bfc6120ed11a1bfa223
윤예지 <hallo27@naver.com>
2023-09-30 23:31:56 +0900

    fix: 자녀 소비지도 마커 등 이전 정보 초기화 안 되는 문제 해결 (#S09P22C207-297)

--- client/lib/screens/allowance_ledger_page/child_spending_route_page.dart
+++ client/lib/screens/allowance_ledger_page/child_spending_route_page.dart
@@ -6,6 +6,7 @@ import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:keeping/provider/account_info_provider.dart';
 import 'package:keeping/provider/child_info_provider.dart';
 import 'package:keeping/provider/user_info.dart';
+import 'package:keeping/screens/allowance_ledger_page/allowance_ledger_page.dart';
 import 'package:keeping/screens/allowance_ledger_page/utils/allowance_ledger_future_methods.dart';
 import 'package:keeping/screens/allowance_ledger_page/widgets/floating_date_btn.dart';
 import 'package:keeping/util/display_format.dart';
@@ -20,7 +21,7 @@ class ChildSpendingRoutePage extends StatefulWidget {
 }
 
 class _ChildSpendingRoutePageState extends State<ChildSpendingRoutePage> {
-  late KakaoMapController mapController;
+  KakaoMapController? mapController;
 
   bool? _parent;
   String? _accessToken;
@@ -75,99 +76,105 @@ class _ChildSpendingRoutePageState extends State<ChildSpendingRoutePage> {
         ),
       ),
       home: Scaffold(
-          appBar: MyHeader(
-            text: '소비경로',
-            bgColor: Colors.purple,
-            elementColor: Colors.white,
+        appBar: MyHeader(
+          text: '소비경로',
+          bgColor: const Color(0xFF8320E7),
+          elementColor: Colors.white,
+          backPath: AllowanceLedgerPage(),
+        ),
+        body: FutureBuilder(
+          future: getAccountListByDate(
+            accessToken: _accessToken, 
+            memberKey: _memberKey,
+            targetKey: _parent != null && _parent! ? _childKey : _memberKey,
+            accountNumber: _parent != null && _parent! ? _childAccountNumber : _accountNumber, 
+            date: DateFormat('yyyy-MM-dd').format(_date),
           ),
-          body: FutureBuilder(
-            future: getAccountListByDate(
-              accessToken: _accessToken, 
-              memberKey: _memberKey,
-              targetKey: _parent != null && _parent! ? _childKey : _memberKey,
-              accountNumber: _parent != null && _parent! ? _childAccountNumber : _accountNumber, 
-              date: DateFormat('yyyy-MM-dd').format(_date),
-            ),
-            builder: (context, snapshot) {
-              mapController.clear();
-              if (snapshot.hasData) {
-                print('소비경로 페이지 ${snapshot.data}');
-                var response = snapshot.data;
-                if (response['resultBody'].isEmpty) {
-                  return Stack(
-                    alignment: Alignment.topCenter,
-                    children: [
-                      KakaoMap(
-                        onMapCreated: ((controller) async {
-                          _onMapCreated(controller);
-                        }),
-                      ),
-                    FloatingDateBtn(setDate: _setDate, selectedDate: _date,)  // 커스텀 위젯
-                    ]
-                  );
-                }
-                print('소비지도 응답값 ${response['resultBody'].values.first}');
-                mapController.setCenter(
-                  LatLng(response['resultBody'].values.first[0]['latitude'] as double, response['resultBody'].values.first[0]['longitude'] as double)
-                );
+          builder: (context, snapshot) {
+            mapController?.clear();
+            if (snapshot.hasData) {
+              print('소비경로 페이지 ${snapshot.data}');
+              var response = snapshot.data;
+              if (response['resultBody'].isEmpty) {
                 return Stack(
                   alignment: Alignment.topCenter,
                   children: [
                     KakaoMap(
                       onMapCreated: ((controller) async {
-                        _onMapCreated(controller);
-                      }),
-                      markers: [
-                        ...response['resultBody'].values.first.map(
-                          (e) => Marker(
-                            markerId: e['storeName'] as String,
-                            latLng: LatLng(
-                              e['latitude'] as double,
-                              e['longitude'] as double,
-                            ),
-                          ),
-                        ),
-                      ],
-                      customOverlays: [
-                        ...response['resultBody'].values.first.map(
-                          (e) => CustomOverlay(
-                            customOverlayId: e['storeName'], 
-                            latLng: LatLng(
-                              double.parse((e['latitude']+0.0005).toString()),
-                              e['longitude'],
-                            ), 
-                            content: '<div style="width: 200px; height: 60px; border-radius: 20px; border: 1px solid #F0F0F0; background: #FFF;"><span style="font-size: 18px; font-weight: 500;">${e['storeName']}</span></br><span style="color: #696969; font-size: 15px;">${formattedTime(DateTime.parse(e['createdDate']))}</span></div>'
-                          )
-                        )
-                      ],
-                      polylines: [
-                        Polyline(
-                          polylineId: _date.toString(),
-                          points: [...response['resultBody'].values.first.map((e) => LatLng(e['latitude'] as double , e['longitude'] as double))],
-                          strokeColor: Colors.blue,
-                          strokeStyle: StrokeStyle.dashDot
-                        )
-                      ],
-                      zoomControl: true,
-                    ),
-                    FloatingDateBtn(setDate: _setDate, selectedDate: _date,)  // 커스텀 위젯
-                  ],
-                );
-              } else {
-                return Stack(
-                  alignment: Alignment.topCenter,
-                  children: [
-                    KakaoMap(
-                      onMapCreated: ((controller) async {
+                        controller.clear();
                         _onMapCreated(controller);
                       }),
                     ),
-                    FloatingDateBtn(setDate: _setDate, selectedDate: _date,)  // 커스텀 위젯
+                  FloatingDateBtn(setDate: _setDate, selectedDate: _date,)  // 커스텀 위젯
                   ]
                 );
               }
+              print('소비지도 응답값 ${response['resultBody'].values.first}');
+              mapController?.setCenter(
+                LatLng(response['resultBody'].values.first[0]['latitude'] as double, response['resultBody'].values.first[0]['longitude'] as double)
+              );
+              return Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  KakaoMap(
+                    onMapCreated: ((controller) async {
+                      controller.clear();
+                      _onMapCreated(controller);
+                    }),
+                    markers: [
+                      ...response['resultBody'].values.first.map(
+                        (e) => Marker(
+                          markerId: e['storeName'] as String,
+                          latLng: LatLng(
+                            e['latitude'] as double,
+                            e['longitude'] as double,
+                          ),
+                        ),
+                      ),
+                    ],
+                    customOverlays: [
+                      ...response['resultBody'].values.first.map(
+                        (e) => CustomOverlay(
+                          customOverlayId: e['storeName'], 
+                          latLng: LatLng(
+                            // double.parse((e['latitude']+0.0005).toString()),
+                            e['latitude'],
+                            e['longitude'],
+                          ), 
+                          yAnchor: 1.6,
+                          content: '<div style="height: 60px; padding: 10px; display: flex; align-items: center; border-radius: 20px; border: 2px solid #F0F0F0; background: #FFF;"><div><span style="font-size: 18px; font-weight: 500;">${e['storeName']}</span></br><span style="color: #696969; font-size: 15px;">${formattedTime(DateTime.parse(e['createdDate']))}</span></div></div>'
+                        )
+                      )
+                    ],
+                    polylines: [
+                      Polyline(
+                        polylineId: _date.toString(),
+                        points: [...response['resultBody'].values.first.map((e) => LatLng(e['latitude'] as double , e['longitude'] as double))],
+                        strokeColor: const Color(0xFF8320E7),
+                        strokeStyle: StrokeStyle.dashDot
+                      )
+                    ],
+                    zoomControl: true,
+                  ),
+                  FloatingDateBtn(setDate: _setDate, selectedDate: _date,)  // 커스텀 위젯
+                ],
+              );
+            } else {
+              return Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  KakaoMap(
+                    onMapCreated: ((controller) async {
+                      controller.clear();
+                      _onMapCreated(controller);
+                    }),
+                  ),
+                  FloatingDateBtn(setDate: _setDate, selectedDate: _date,)  // 커스텀 위젯
+                ]
+              );
             }
-          )
+          }
+        )
       ),
     );
   }
