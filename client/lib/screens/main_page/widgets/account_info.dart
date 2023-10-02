7780f7ce67842dfd29edd279eb83a3b9b3540742
윤예지 <hallo27@naver.com>
2023-10-02 23:49:50 +0900

    feat: 부모 메인페이지 용돈 보내가 경로 추가

--- client/lib/screens/main_page/widgets/account_info.dart
+++ client/lib/screens/main_page/widgets/account_info.dart
@@ -8,6 +8,7 @@ import 'package:keeping/screens/allowance_ledger_page/utils/allowance_ledger_fut
 import 'package:keeping/screens/make_account_page/make_account_page.dart';
 import 'package:keeping/screens/parent_accept_request_money/parent_accept_request_money.dart';
 import 'package:keeping/screens/request_pocket_money_page/child_request_money_page.dart';
+import 'package:keeping/screens/send_pocket_money_page/send_pocket_money_page.dart';
 import 'package:keeping/styles.dart';
 import 'package:keeping/util/display_format.dart';
 import 'package:provider/provider.dart';
@@ -142,7 +143,7 @@ class _AccountInfoState extends State<AccountInfo> {
                         Expanded(
                           child: InkWell(
                             onTap: () {
-                        
+                              Navigator.push(context, MaterialPageRoute(builder: (_) => SendPocketMoneyPage()),);
                             },
                             child: Text(
                               '용돈 보내기', 
