a7ddcf82bd92d6bde077323799cde90d5ea4c6db
양수원 <classicblue205@gmail.com>
2023-10-02 23:08:34 +0900

    feat: 조르기 거부 및 승인 (#S09P22C207-357)

--- client/lib/screens/main_page/widgets/account_info.dart
+++ client/lib/screens/main_page/widgets/account_info.dart
@@ -7,6 +7,7 @@ import 'package:keeping/screens/allowance_ledger_page/allowance_ledger_page.dart
 import 'package:keeping/screens/allowance_ledger_page/utils/allowance_ledger_future_methods.dart';
 import 'package:keeping/screens/make_account_page/make_account_page.dart';
 import 'package:keeping/screens/request_pocket_money_page/child_request_money_page.dart';
+import 'package:keeping/screens/send_pocket_money_page/send_pocket_money_page.dart';
 import 'package:keeping/styles.dart';
 import 'package:keeping/util/display_format.dart';
 import 'package:provider/provider.dart';
@@ -44,7 +45,8 @@ class _AccountInfoState extends State<AccountInfo> {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (_) => AllowanceLedgerPage()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (_) => AllowanceLedgerPage()));
       },
       child: Padding(
         padding: const EdgeInsets.only(top: 24, bottom: 12),
@@ -60,48 +62,52 @@ class _AccountInfoState extends State<AccountInfo> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Image.asset('assets/image/money_coins.png', width: 100,),
+                      Image.asset(
+                        'assets/image/money_coins.png',
+                        width: 100,
+                      ),
                       Expanded(
-                        child: Column(
-                          children: [
-                            Text(
-                              formattedMoney(widget.balance),
-                              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                          child: Column(
+                        children: [
+                          Text(
+                            formattedMoney(widget.balance),
+                            style: TextStyle(
+                                fontSize: 26, fontWeight: FontWeight.bold),
+                          ),
+                          FutureBuilder(
+                            future: getMonthTotalExpense(
+                              accessToken: _accessToken,
+                              memberKey: _memberKey,
+                              targetKey: _parent != null && _parent!
+                                  ? _childKey
+                                  : _memberKey,
+                              date:
+                                  DateFormat('yyyy-MM').format(DateTime.now()),
                             ),
-                            FutureBuilder(
-                              future: getMonthTotalExpense(
-                                accessToken: _accessToken,
-                                memberKey: _memberKey,
-                                targetKey: _parent != null && _parent! ? _childKey : _memberKey,
-                                date: DateFormat('yyyy-MM').format(DateTime.now()),
-                              ), 
-                              builder: (context, snapshot) {
-                                if (snapshot.hasData) {
-                                  if (snapshot.data['resultStatus']['resultCode'] == '503') {
-                                    return Text(
-                                      '${DateTime.now().month}월 총 지출액: 0원',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Color(0xFF737373)
-                                      ),
-                                    );
-                                  }
-                                  var response = snapshot.data['resultBody'];
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                if (snapshot.data['resultStatus']
+                                        ['resultCode'] ==
+                                    '503') {
                                   return Text(
-                                    '${DateTime.now().month}월 총 지출액: ${formattedMoney(response)}',
+                                    '${DateTime.now().month}월 총 지출액: 0원',
                                     style: TextStyle(
-                                      fontSize: 14, 
-                                      color: Color(0xFF737373)
-                                    ),
+                                        fontSize: 14, color: Color(0xFF737373)),
                                   );
-                                } else {
-                                  return Text('로딩중');
                                 }
-                              },
-                            ),
-                          ],
-                        )
-                      )
+                                var response = snapshot.data['resultBody'];
+                                return Text(
+                                  '${DateTime.now().month}월 총 지출액: ${formattedMoney(response)}',
+                                  style: TextStyle(
+                                      fontSize: 14, color: Color(0xFF737373)),
+                                );
+                              } else {
+                                return Text('로딩중');
+                              }
+                            },
+                          ),
+                        ],
+                      ))
                     ],
                   ),
                 ),
@@ -110,62 +116,82 @@ class _AccountInfoState extends State<AccountInfo> {
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Container(height: 1.2, color: Color.fromARGB(255, 204, 204, 204),),
+                  child: Container(
+                    height: 1.2,
+                    color: Color.fromARGB(255, 204, 204, 204),
+                  ),
                 ),
-                _parent != null && !_parent! ? 
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (_) {
-                          if (_accountNumber != '') {
-                            return ChildRequestMoneyPage();
-                          } else {
-                            return MakeAccountPage();
-                          }
-                        },
-                      ),);
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: Text(
-                        '용돈 조르기',
-                        style: TextStyle(color: Color(0xFFA5A5A5), fontSize: 14),
-                      )
-                    ),
-                  )
-                :
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: InkWell(
-                            onTap: () {
-                        
-                            },
-                            child: Text(
-                              '용돈 보내기', 
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: Color(0xFFA5A5A5), fontSize: 14),
+                _parent != null && !_parent!
+                    ? InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) {
+                                if (_accountNumber != '') {
+                                  return ChildRequestMoneyPage();
+                                } else {
+                                  return MakeAccountPage();
+                                }
+                              },
                             ),
-                          ),
-                        ),
-                        Container(width: 1.2, height: 30, color: Color.fromARGB(255, 204, 204, 204),),
-                        Expanded(
-                          child: InkWell(
-                            onTap: () {
-                        
-                            },
+                          );
+                        },
+                        child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
                             child: Text(
-                              '조르기 모아보기', 
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: Color(0xFFA5A5A5), fontSize: 14),
+                              '용돈 조르기',
+                              style: TextStyle(
+                                  color: Color(0xFFA5A5A5), fontSize: 14),
+                            )),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: InkWell(
+                                
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (_) {
+                                        if (_accountNumber != '') {
+                                          return SendPocketMoneyPage();
+                                        } else {
+                                          return MakeAccountPage();
+                                        }
+                                      },
+                                    ),
+                                  );
+                                },
+                                child: Text(
+                                  '용돈 보내기',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Color(0xFFA5A5A5), fontSize: 14),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: 1.2,
+                              height: 30,
+                              color: Color.fromARGB(255, 204, 204, 204),
+                            ),
+                            Expanded(
+                              child: InkWell(
+                                  onTap: () {},
+                                  child: Text(
+                                    '조르기 모아보기',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Color(0xFFA5A5A5), fontSize: 14),
+                                  )),
                             )
-                          ),
-                        )
-                      ],
-                    ),
-                  )
+                          ],
+                        ),
+                      )
               ],
             ),
           ),
@@ -173,4 +199,5 @@ class _AccountInfoState extends State<AccountInfo> {
       ),
     );
   }
+  
 }
