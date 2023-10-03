d3b09e491c54d07df38bd368e39549d09031bbda
윤예지 <hallo27@naver.com>
2023-10-04 00:58:08 +0900

    feat: 헤더 위젯 뒤로 가기 화살표 없앨 수 있게 수정

--- client/lib/widgets/header.dart
+++ client/lib/widgets/header.dart
@@ -9,6 +9,7 @@ class MyHeader extends StatelessWidget implements PreferredSizeWidget {
   final Widget? icon; // 오른쪽 상단에 들어갈 아이콘 (ex. Icon(Icons.arrow_back))
   final dynamic iconPath; // 오른쪽 상단 아이콘을 클릭했을 때 이동할 곳
   final dynamic path;
+  final bool backIcon;
 
   MyHeader({
     super.key,
@@ -19,6 +20,7 @@ class MyHeader extends StatelessWidget implements PreferredSizeWidget {
     this.icon,
     this.iconPath,
     this.path,
+    this.backIcon = true,
   });
 
   @override
@@ -36,13 +38,13 @@ class MyHeader extends StatelessWidget implements PreferredSizeWidget {
               height: statusBarSize + 5,
             ),
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              backBtn(context, elementColor, backPath),
+              (backIcon) // 오른쪽 아이콘이 없을 경우 SizedBox를 추가해 정렬에 문제없도록 처리
+                ? backBtn(context, elementColor, backPath)
+                : SizedBox(width: 55, height: 55,),
               titleText(context, elementColor, text),
               (icon != null) // 오른쪽 아이콘이 없을 경우 SizedBox를 추가해 정렬에 문제없도록 처리
-                  ? extraBtn(context, elementColor, icon!, iconPath)
-                  : SizedBox(
-                      width: 55,
-                    )
+                ? extraBtn(context, elementColor, icon!, iconPath)
+                : SizedBox(width: 55,)
             ]),
             SizedBox(
               height: 5,
@@ -64,7 +66,7 @@ Widget backBtn(BuildContext context, Color elementColor, dynamic backPath) {
         Navigator.pop(context);
       }
     },
-    icon: Icon(Icons.arrow_back),
+    icon: Icon(Icons.arrow_back_rounded),
     color: elementColor,
     iconSize: 40.0,
   );
