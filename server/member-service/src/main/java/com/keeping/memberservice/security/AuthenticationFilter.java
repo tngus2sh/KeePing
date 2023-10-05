2e3c080345126b6e3dbbcf405bc75dfe41ca2bc0
이예리 <leeyr0412@naver.com>
2023-10-05 15:39:45 +0900

    feat: 빌드 수정

--- server/member-service/src/main/java/com/keeping/memberservice/security/AuthenticationFilter.java
+++ server/member-service/src/main/java/com/keeping/memberservice/security/AuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.keeping.memberservice.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.keeping.memberservice.api.ApiResponse;
 import com.keeping.memberservice.api.controller.request.LoginRequest;
 import com.keeping.memberservice.api.service.member.MemberService;
 import com.keeping.memberservice.domain.Member;
@@ -10,6 +11,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,6 +20,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -46,24 +49,53 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
             LoginRequest creds = new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
 
             return getAuthenticationManager()
-                .authenticate(new UsernamePasswordAuthenticationToken(creds.getLoginId(), creds.getLoginPw(), new ArrayList<>()));
+                    .authenticate(new UsernamePasswordAuthenticationToken(creds.getLoginId(), creds.getLoginPw(), new ArrayList<>()));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+//        super.unsuccessfulAuthentication(request, response, failed);
+        setResponse(response, false);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException {
         String username = ((User) authResult.getPrincipal()).getUsername();
         Member member = memberService.getUserDetailsByLoginId(username);
 
         String token = Jwts.builder()
-            .setSubject(member.getMemberKey())
-            .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-            .signWith(key, SignatureAlgorithm.HS256)
-            .compact();
+                .setSubject(member.getMemberKey())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
 
         response.addHeader("token", token);
         response.addHeader("memberKey", member.getMemberKey());
+
+        setResponse(response, true);
+    }
+
+    private void setResponse(HttpServletResponse response, boolean result) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ApiResponse<String> loginResponse;
+
+        if (result) {
+            loginResponse = ApiResponse.ok("로그인 성공");
+        } else {
+            loginResponse = ApiResponse.of(1, HttpStatus.BAD_REQUEST, "아이디 혹은 비밀번호를 확인하세요.");
+        }
+        String login = objectMapper.writeValueAsString(loginResponse);
+
+        response.getWriter().write(login);
     }
 }
